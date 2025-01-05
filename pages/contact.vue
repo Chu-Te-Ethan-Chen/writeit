e6842948ff0a2fdb21f463f79f6c95b99408c5b5
Ethan Chen <ethan-chen@Ethans-MacBook-Pro-3.local>
2025-01-05 14:55:22 -0500

    Updated contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="contact-page" class="page-wrapper contact-page content-page">
     <site-hero
-      title="Contact Us"
-      subtitle="Example Contact Page"
+      title="Contact"
       image="/uploads/contact-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
